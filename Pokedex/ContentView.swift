863fe342334fd68d990628d03c00c54317fcfeb9
Etienne Vautherin <etienne@vautherin.com>
2024-02-08 15:19:16 +0100

    First commit

--- Pokedex/ContentView.swift
+++ Pokedex/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    let model = ApiModel.shared
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
---
+++ Pokedex/PokemonLink.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonLink.swift
+//  Pokedex
+//
+//  Created by Etienne Vautherin on 08/02/2024.
+//
+
+import Foundation
+
+struct PokemonLink: Codable {
+    let name: String
+    let url: String
+}
